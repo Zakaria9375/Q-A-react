e9693b2c2eba742e56c3ebb2c391f12bbc139572
Zakaria Ali <115387468+Zakaria9375@users.noreply.github.com>
2024-07-02 15:14:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,19 +7,12 @@ import { getConfig } from "./authConfig.ts";
 import { Provider } from "react-redux";
 import { store } from "./stores/store.ts";
 const { domain, clientId } = getConfig();
-function onRedirect() {
-	window.history.replaceState(
-		{},
-		document.title,
-		window.location.origin + "/redirect"
-	);
-}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Auth0Provider
 			domain={domain}
 			clientId={clientId}
-			onRedirectCallback={onRedirect}
 		>
 			<Provider store={store}>
 				<App />
